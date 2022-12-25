0d79567e50471c7ff62147b105a285bbcfc73a9e
faragzz <91439764+faragzz@users.noreply.github.com>
2022-12-25 22:13:14 +0200

    second commit

--- app/src/main/java/com/example/egfwrd11/Communicator1.kt
+++
@@ -1,4 +0,0 @@
-package com.example.egfwrd11
-interface Communicator1 {
-    fun passData(editTextInput :String)
-}
--- app/src/main/java/com/example/egfwrd11/ShoeDetailTransfer.kt
+++
@@ -1,5 +0,0 @@
-package com.example.egfwrd11
-
-interface ShoeDetailTransfer {
-    fun ShoeData (name:String,comp:String,size:String,desc:String)
-}
